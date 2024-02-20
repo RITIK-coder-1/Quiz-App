3d626ef6d0c53f0bffdd9a2d042303987ad92610
RITIK <ritikprofessional111@gmail.com>
2024-02-20 22:09:05 +0530

    My quiz is almost completed

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let scoreNumber = 0 // it tracks the score
-let optionChoosen = false // it looks if any of the options is chosen or not
+let optionChosen = false // it looks if any of the options is chosen or not
+let numberOfQuestion = 1 // it tracks the number of questions
+let randomNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] // it is used to get a random number to display different questions
 
 // Selecting Elements -->
 
@@ -7,6 +9,9 @@ const options = document.getElementsByClassName("options")
 const question = document.getElementById("question")
 let score = document.getElementById("score")
 const next = document.getElementById("next")
+const questionNumber = document.getElementById("questionNumber")
+const header = document.querySelector("header")
+const main = document.querySelector("main")
 
 // Fetching API --->
 
@@ -15,22 +20,105 @@ questionsData
 .then(Response => Response.json())
 .then(data => {
     console.log(data);
-    const results = data.results[randomQuestion()]
-    console.log(results);
-    const questions = results.question
-    console.log(questions);
-    const correct = results.correct_answer
-    console.log(correct);
-    const incorrect = results.incorrect_answers
-    console.log(incorrect);
-    const answers = [correct, ...incorrect]
-    console.log(answers);
 
-    question.innerText = `${questions}`
+    // data is in JSON format, it has a "results" property that is an array with different questions as objects
 
-    answers.sort(() => Math.random() - 0.5)
+    let results = data.results[0] // it stores the first object of the results array 
+    let questions = results.question // it stores the question of the first object
+    let correct = results.correct_answer // it stores the correct answer of the first question
+    let incorrect = results.incorrect_answers // it stores the incorrect answers that is again an array
+    let answers = [correct, ...incorrect] // it stores the destructured elements as a single answer
+
+    question.innerText = `${questions}` // the question is displayed on the page
+
+    answers.sort(() => Math.random() - 0.5) // the array is sorted in order to shuffle the elements randomly so that no two options get the same answer
+
+    // options are displayed on the page -->
+
+    options[0].innerText = `${answers[0]}` 
+    options[1].innerText = `${answers[1]}`
+    options[2].innerText = `${answers[2]}`
+    options[3].innerText = `${answers[3]}`
+
+    Array.from(options).forEach(ele => {
+
+        // when an option is selected -->
+
+        ele.addEventListener("click", () => {
+            if (!optionChosen) {
+
+                // if no option is chosen then it will perfrom the action -->
+
+                if (ele.innerText === correct) {
+                    ele.style.border = "2px solid green" // correct answer gets green
+                    ele.style.color = "green"
+                    scoreNumber++ // score number is increased
+                    score.innerText = `Score: ${scoreNumber}` // the score is updated on the page correspondingly
+                } else {
+                    ele.style.border = "2px solid red" // incorrect answer gets red
+                    ele.style.color = "red"
+                }
+            }
+            optionChosen = true // once an option is selected, the option chosen becomes true
+        })
+    })
+    
+    next.addEventListener("click", () => {
+
+        // it will take to the next question only if an option has been selected -->
+
+        if (optionChosen) {
+            randomNumber.sort(() => Math.random() - 0.5) // the array is sorted in order to shuffle the elements randomly so that no two questions are the same
+            
+            // all the values are updated accordingly -->
+
+            results = data.results[randomNumber[0]]
+            questions = results.question
+            correct = results.correct_answer
+            incorrect = results.incorrect_answers
+            answers = [correct, ...incorrect]
+            question.innerText = `${questions}`
+    
+            numberOfQuestion++ // the question number is increased
+    
+            questionNumber.innerText = `Question: ${numberOfQuestion}/10` // it is updated on the page
+            
+            answers.sort(() => Math.random() - 0.5)            
+    
+            options[0].innerText = `${answers[0]}`
+            options[1].innerText = `${answers[1]}`
+            options[2].innerText = `${answers[2]}`
+            options[3].innerText = `${answers[3]}`
+
+            // the options get back to their default style -->
+
+            Array.from(options).forEach(ele => {
+                ele.style.border = "1px solid #1d1c1c"
+                ele.style.color = "black"
+            }) 
+
+            optionChosen = false // option chosen gets its initial value back 
+
+            // once it reaches the 10th question, the next page displays the result -->
+            
+            if (numberOfQuestion === 10) {
+                next.addEventListener("click", () => {
+                    header.remove()
+                    main.remove()
+                    let h1 = document.createElement("h1")
+                    h1.innerText = `Your Score is: ${scoreNumber}`
+                    h1.style.color = "black"
+                    document.body.append(h1)
+                    document.body.style.display = "flex"
+                    document.body.style.justifyContent = "center"
+                    document.body.style.alignItems = "center"
+                    document.body.style.fontFamily = "roboto"
+                })
+            }
+        }
+    })        
 })
 
-function randomQuestion() {
-    return Math.floor(Math.random() * 10)
+function valueAssigment() {
+    
 }
